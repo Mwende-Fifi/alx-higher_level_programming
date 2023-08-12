f01c4a2a855a3980390c268ef50c12e47a8d132c
Mwende-Fifi <mwendekiindu@gmail.com>
2023-08-12 11:44:24 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-alphabet = ""
+
+def print_alphabet(exclude):
+    """Prints the alphabets, except for the letters in the exclude list."""
+
+if __name__ == "__main__":
+    alphabet = ""
 for i in range(97, 123):
     if i != 101 and i != 113:
+        exclude = [101, 113]
         if i not in exclude:
             alphabet += chr(i)
             if alphabet:
-                print(f"{alphabet}"
-
-
-                    if __name__ == "__main__":
-                    exclude = [101, 113]
-                    print_alphabet(exclude)
+                print(f"{alphabet}")
+                print_alphabet(exclude)
