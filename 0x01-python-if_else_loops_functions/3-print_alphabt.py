eee43d9216834e4980856421ef6c389a3dae25f8
Mwende-Fifi <mwendekiindu@gmail.com>
2023-08-12 11:53:31 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
-def print_alphabet(exclude):
-    """Prints the alphabets, except for the letters in the exclude list."""
-
-if __name__ == "__main__":
-    alphabet = ""
 for i in range(97, 123):
-    if i != 101 and i != 113:
-        exclude = [101, 113]
-        if i not in exclude:
-            alphabet += chr(i)
-            if alphabet:
-                print(f"{alphabet}")
-                print_alphabet(exclude)
+    if (i != 101 and i != 113):
+        print("{:c}".format(i), end='')
