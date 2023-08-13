194889c456867040bfc3d4c47207b565fdc3a2f3
Mwende-Fifi <mwendekiindu@gmail.com>
2023-08-13 06:52:09 -0700

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,10 @@ import sys
 if __name__ == '__main__':
     av = sys.argv
     l_av = len(av)
-    sum = 0
+    total_sum = 0
 
     if l_av > 1:
         for i in range(1, l_av):
-            sum += int(av[i])
-            print(sum)
+            total_sum += int(av[i])
+
+            print(total_sum)
